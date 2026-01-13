d2fbcafc2dea17bd8474e4f0b7dd6626e6e07695
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2026-01-14 00:56:09 +0530

    learn more about backend

--- 03_RestFullApi/HowToServe_StaticFile.js
+++ 03_RestFullApi/HowToServe_StaticFile.js
@@ -4,6 +4,11 @@ const app = express()
 
 const PORT = process.env.PORT || 8000
 
+app.use(express.static('public'))
+
+app.get("/",(req,res)=>{
+    res.end("Hello world")
+})
 
 
 app.listen(PORT,()=>{
